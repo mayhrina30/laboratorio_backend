06ba690a727172b75ac26cb3f09f8036f92c2403
mayra manzaneda <urielortega300@gmail.com>
2023-09-08 16:56:44 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import app from "./app"; 
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`conexion establecida ${port}`);
